d0b95616341b0339ff4998aba8337fc1db1e3293
Sondos Mohamed <39069270+So47@users.noreply.github.com>
2024-02-27 23:23:30 +0100

    Update main.sql

--- main.sql
+++ main.sql
@@ -1,3 +1,5 @@
-# Write your MySQL query statement below
-SELECT Email FROM Person
-GROUP BY Email HAVING COUNT(*) > 1;
+-- # Write your MySQL query statement below
+-- SELECT Email FROM Person
+-- GROUP BY Email HAVING COUNT(*) > 1;
+SELECT DISTINCT P1.email From Person P1, Person P2
+WHERE P1.id <> P2.id AND P1.email = P2.email
